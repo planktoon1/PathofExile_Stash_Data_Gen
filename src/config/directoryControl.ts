974fcd02b64dbe65c5424e66725cd1e7befd2318
Frederik Østergaard <fkoe90@gmail.com>
2020-03-26 11:31:43 +0100

    Update RePoE Data automation

--- src/config/directoryControl.ts
+++ src/config/directoryControl.ts
@@ -2,5 +2,6 @@
 
 export const enum Directories {
   OUTPUT_DIR_readable = `./output/readable`,
-  OUTPUT_DIR_minified = `./output/minified`
+  OUTPUT_DIR_minified = `./output/minified`,
+  INPUT_DIR_RePoE = `./input/RePoE_Data`
 }
--- src/utility/updateInputData/updateRePoEData.ts
+++ src/utility/updateInputData/updateRePoEData.ts
@@ -1,9 +1,46 @@
-export const updateRePoEData = () => {
+import git from "isomorphic-git";
+import http from "isomorphic-git/http/node";
+import fs from "fs";
+import rimraf from "rimraf";
+import { Directories } from "../../config/directoryControl";
+export const updateRePoEData = async () => {
+  console.log(`🌟 Updating RePoe Data 🌟`);
+  console.log(`   Deleting old RePoE Data...`);
+  rimraf.sync(Directories.INPUT_DIR_RePoE + "/");
   // fetch repoe data
-  // put data in input folder
+  console.log(`   Fetching new RePoE Data...`);
+  await fetchRePoEData();
+  console.log(`🌟 Done updating RePoE Data 🌟`);
 };
 
-const fetchRePoEData = () => {
+const fetchRePoEData = async () => {
   // call github
-  // only get specific folder inside repo
+  await git.clone({
+    fs,
+    http,
+    dir: Directories.INPUT_DIR_RePoE,
+    url: "https://github.com/brather1ng/RePoE.git",
+    singleBranch: true,
+    depth: 1,
+    remote: "RePoE",
+    onProgress: logGitCloneProgress
+  });
 };
+
+const logGitCloneProgress = onProgressParam => {
+  if (
+    onProgressParam &&
+    onProgressParam.total &&
+    onProgressParam.loaded &&
+    onProgressParam.phase === "Updating workdir"
+  ) {
+    const percentage = Math.round(
+      (onProgressParam.loaded / onProgressParam.total) * 100
+    );
+    if (percentage % 10 === 0) {
+      console.log(`   Cloning RePoE Repo: ${percentage}%`);
+    }
+  }
+};
+
+updateRePoEData();
